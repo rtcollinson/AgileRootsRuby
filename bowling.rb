4c28e7a0aa063f7477228b15f266bd90dfba619b
scott <scott.d.peterson@hp.com>
2014-06-02 23:59:59 -0600

    Almost done.

--- bowling.rb
+++ bowling.rb
@@ -1,40 +1,67 @@
 class BowlingScore
 
   def initialize
-  @current_score = nil
-  @game_frame = Array.new(10) {Array.new(2)}
-  @frame = 0
-  @roll = 0
+    @game_frame = Array.new(10) { Array.new(3) }
+    @frame = 0
+    @roll = 0
   end
 
   def next_score score
     if score < 11
-      @current_score = score
-      if score == 10
-        @game_frame[@frame][@roll] = @current_score
-        @frame = @frame + 1
-      elsif @roll == 0
-        @game_frame[@frame][@roll] = @current_score
-        @roll = 1
-      elsif @roll == 1
-        @game_frame[@frame][@roll] = @current_score
-        @roll = 0
-        @frame = @frame + 1
-
-      else
+      if @frame == 9
+        if @roll < 2 && score < 11
+          if @game_frame[@frame][0] != nil && @game_frame[@frame][0] + score < 11
+            @game_frame[@frame][@roll] = score
+            @roll = @roll + 1
+            return true
+          elsif @game_frame[@frame][0] == nil || @game_frame[@frame][0] == 10
+            @game_frame[@frame][@roll] = score
+            @roll = @roll + 1
+            return true
+          end
+        elsif @roll == 2 && @game_frame[@frame][0] != nil && @game_frame[@frame][1] != nil &&  @game_frame[@frame][0] + @game_frame[@frame][1] > 9
+          @game_frame[@frame][@roll] = score
+          return true
+        end
         return false
       end
-      return true
+      unless @roll == 1 && @game_frame[@frame][0] + score > 10
+        if score == 10 && @roll == 0
+          @game_frame[@frame][@roll] = score
+          @frame = @frame + 1
+        elsif @roll == 0
+          @game_frame[@frame][@roll] = score
+          @roll = 1
+        elsif @roll == 1
+          @game_frame[@frame][@roll] = score
+          @roll = 0
+          @frame = @frame + 1
+        else
+          return false
+        end
+        return true
+      end
     end
     false
   end
 
-  def last_score
-    @current_score
-  end
-
   def frame_score frame
     frame_score = @game_frame[frame]
+    if frame_score[0] == 10
+      if @game_frame[frame + 1][0] == 10 && @game_frame[frame + 2][0] == 10
+        return frame_score[0] + @game_frame[frame + 1][0] + @game_frame[frame + 2][0]
+      elsif @game_frame[frame + 1][0] == 10 && @game_frame[frame + 2][0] != nil && @game_frame[frame + 2][0] < 10
+        return frame_score[0] + @game_frame[frame + 1][0] + @game_frame[frame + 2][0]
+      elsif @game_frame[frame + 1][0] != nil && @game_frame[frame + 1][0] < 10 && @game_frame[frame + 1][1] != nil && @game_frame[frame + 1][1] < 10
+        return frame_score[0] + @game_frame[frame + 1][0] + @game_frame[frame + 1][1]
+      end
+      return "X"
+    elsif frame_score[0] != nil && frame_score[1] != nil && frame_score[0] < 10 && frame_score[0] + frame_score[1] == 10
+      if @game_frame[frame + 1][0] != nil
+        return frame_score[0] + frame_score[1] + @game_frame[frame + 1][0]
+      end
+      return "/"
+    end
     (frame_score[0] == nil ? 0 : frame_score[0]) + (frame_score[1] == nil ? 0 : frame_score[1])
   end
 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -10,14 +10,14 @@ describe BowlingScore do
     @bowling_score.should_not be_nil
   end
 
-  it "should accept the result of a roll and return that score as last score" do
+  it "should accept the result of a roll and return that score as the frame score" do
     @bowling_score.next_score(1).should be_true
-    @bowling_score.last_score.should == 1
+    @bowling_score.frame_score(0).should == 1
   end
 
-  it "should accept a score of 2 and last score should be 2" do
+  it "should accept a score of 2 and frame score should be 2" do
     @bowling_score.next_score(2).should be_true
-    @bowling_score.last_score.should == 2
+    @bowling_score.frame_score(0).should == 2
   end
 
   it "Score for a single roll should not be more than 10" do
@@ -28,35 +28,127 @@ describe BowlingScore do
     @bowling_score.frame_score(0).should == 0
   end
 
-  it "should return a 1 after the first '1' roll in a frame" do
+  it "should return the correct score for a frame under 10 pins" do
     @bowling_score.next_score(1)
     @bowling_score.frame_score(0).should == 1
     @bowling_score.next_score(3)
     @bowling_score.frame_score(0).should == 4
     @bowling_score.next_score(5)
     @bowling_score.frame_score(1).should == 5
+    @bowling_score.next_score(3)
+    @bowling_score.frame_score(1).should == 8
+    @bowling_score.next_score(2)
+    @bowling_score.next_score(4)
+    @bowling_score.frame_score(2).should == 6
+    @bowling_score.frame_score(1).should == 8
+    @bowling_score.frame_score(0).should == 4
   end
 
-  #public void BowlingScoreByFrame()
-  #{
-  #    BowlingScore curScore = new BowlingScore();
-  #curScore.nextScore(1);
-  #assertEquals("After one roll, current frame should be 1", 1, curScore.thisFrameScore());
-  #curScore.nextScore(1);
-  #assertEquals("Current frame should have a score of 2", 2, curScore.thisFrameScore());
-  #BowlingScore strikeCase = new BowlingScore();
-  #strikeCase.nextScore(10);
-  #assertEquals("If 10 on first roll, then previous frame should show 10", 10, strikeCase.prevFrame());
-  #
-  #}
-  #
-  #@Test
-  #public void ChangeFrame()
-  #{
-  #    BowlingScore curScore = new BowlingScore();
-  #curScore.nextScore(10);
-  #assertEquals("If strike then it should move to next frame",0,curScore.thisFrameScore());
-  #}
+  it "should switch frames when a strike is rolled" do
+    @bowling_score.next_score(10)
+    @bowling_score.frame_score(0).should == "X"
+  end
+
+  #it "should account for spares when all ten pins are down in a frame" do
+  #  @bowling_score.next_score(10)
+  #  @bowling_score.frame_score(0).should == 10
+  #end
+
+  it "if a frame other than the 10th adds to more than 10 even when individual rolls are less than 10, next_score returns false and doesn't change frame'" do
+    @bowling_score.next_score(9).should be_true
+    @bowling_score.next_score(8).should be_false
+    @bowling_score.next_score(1).should be_true
+    @bowling_score.frame_score(0).should == "/"
+  end
+
+  it "should not show a score for a strike frame until two more rolls have been scored, turkey" do
+    @bowling_score.next_score(10)
+    @bowling_score.frame_score(0).should == "X"
+    @bowling_score.next_score(10)
+    @bowling_score.frame_score(0).should == "X"
+    @bowling_score.next_score(10)
+    @bowling_score.frame_score(0).should == 30
+  end
+
+  it "should not show a score for a strike until two more rolls are scored, one strike , one not" do
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(3)
+    @bowling_score.frame_score(0).should == 23
+  end
+
+  it "should not show a score for a strike until two more rolls are scored, neither strikes" do
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(5)
+    @bowling_score.next_score(2)
+    @bowling_score.frame_score(0).should == 17
+  end
+
+  it "should handle a strike plus a spare" do
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(5)
+    @bowling_score.next_score(5)
+    @bowling_score.frame_score(0).should == 20
+  end
+
+  it "should not score a spare until the next roll after the spare frame" do
+    @bowling_score.next_score(1)
+    @bowling_score.next_score(9)
+    @bowling_score.frame_score(0).should == "/"
+    @bowling_score.next_score(3)
+    @bowling_score.frame_score(0).should == 13
+  end
+
+  it "should not score a spare until the next roll after the spare frame, 0 then 10" do
+    @bowling_score.next_score(0)
+    @bowling_score.next_score(10)
+    @bowling_score.frame_score(0).should == "/"
+    @bowling_score.next_score(3)
+    @bowling_score.frame_score(0).should == 13
+  end
+
+
+end
+
+describe "TenthFrame" do
+  before :each do
+    @bowling_score = BowlingScore.new
+    # Fill the first 9 frames
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+    @bowling_score.next_score(10)
+  end
+
+  it "should allow you to have three strikes in the 10th frame" do
+    @bowling_score.next_score(10).should be_true
+    @bowling_score.next_score(10).should be_true
+    @bowling_score.next_score(10).should be_true
+  end
+
+  it "should handle a spare" do
+    @bowling_score.next_score(5).should be_true
+    @bowling_score.next_score(5).should be_true
+    @bowling_score.next_score(5).should be_true
+  end
+
+  it "if two rolls < 10 dont allow a third roll" do
+    @bowling_score.next_score(5).should be_true
+    @bowling_score.next_score(4).should be_true
+    @bowling_score.next_score(5).should be_false
+  end
+
+  it "should not allow the second roll to allow the frame to add to more than 10" do
+    @bowling_score.next_score(5)
+    @bowling_score.next_score(6).should be_false
+    @bowling_score.next_score(5).should be_true
+    @bowling_score.next_score(5).should be_true
+  end
 
 
 end
